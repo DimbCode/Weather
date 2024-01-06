433905c9780cf502597a581a54ad6910f35eb6a8
Dimbb <bogomolovdmitrij535@gmail.com>
2024-01-06 20:38:15 +0300

    last changes #3

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,39 +3,9 @@
 import { CurrentInfo } from "./modules/CurrentInfo.js";
 import { WeatherQuerie } from "./modules/WeatherQuerie.js";
 import { ChangeLocation } from "./modules/ChangeLocation.js";
+import { info, changeLocation as changeLocationVar } from "./modules/startValues.js";
 
-async function showResult(promise) {
-
-    let result = await promise;
-    console.log(result);
-
-}
-
-const elements = {
-    days: ".details-weather__content-wrapper",
-    day: ".current-weather__week-day",
-    date: ".current-weather__date",
-    place: ".current-weather__address",
-    icon: ".current-weather__icon",
-    temperature: ".current-weather__temperature",
-    status: ".current-weather__status",
-    precipitation: ".details-weather__list-item-value_precipitation",
-    humidity: ".details-weather__list-item-value_humidity",
-    wind: ".details-weather__list-item-value_wind",
-    container: ".weather__container",
-}
-
-const currentInfo = new CurrentInfo({
-    elements,
-    cityLat: 44.1053,
-    cityLon: 39.0802,
-    openClass: "weather__container_open",
-});
-
-const changeLocation = new ChangeLocation({
-    parentElement: ".details-weather__footer-wrapper",
-    openClass: "details-weather__footer-wrapper_open",
-    currentInfo: currentInfo,
-});
+const currentInfo = new CurrentInfo(info);
+const changeLocation = new ChangeLocation({ ...changeLocationVar, currentInfo: currentInfo });
 
 currentInfo.setWeather();
--- assets/js/modules/startValues.js
+++ assets/js/modules/startValues.js
@@ -1,2 +1,27 @@
 // Start Values
 
+export const elements = {
+    days: ".details-weather__content-wrapper",
+    day: ".current-weather__week-day",
+    date: ".current-weather__date",
+    place: ".current-weather__address",
+    icon: ".current-weather__icon",
+    temperature: ".current-weather__temperature",
+    status: ".current-weather__status",
+    precipitation: ".details-weather__list-item-value_precipitation",
+    humidity: ".details-weather__list-item-value_humidity",
+    wind: ".details-weather__list-item-value_wind",
+    container: ".weather__container",
+}
+
+export const info = {
+    elements,
+    cityLat: 55.7522,
+    cityLon: 37.6156,
+    openClass: "weather__container_open",
+}
+
+export const changeLocation = {
+    parentElement: ".details-weather__footer-wrapper",
+    openClass: "details-weather__footer-wrapper_open",
+}
